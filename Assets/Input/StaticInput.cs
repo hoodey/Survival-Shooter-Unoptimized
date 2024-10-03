571ce3d18f0a9d63c3923fe603a40855f4048647
hoodey <41493055+hoodey@users.noreply.github.com>
2024-10-02 19:45:58 -0500

    Player movement updated to use events

---
+++ Assets/Input/StaticInput.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticInput
+{
+    public static IA_Actions controls = new IA_Actions();
+    static StaticInput()
+    {
+
+    }
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,43 +1,73 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
 	public float speed = 6f;
 
 	private Vector3 movement;
+	private Vector2 movementInput;
 	private Animator anim;
 	private Rigidbody playerRigidbody;
 	private int floorMask;
 	private float camRayLength = 100f;
+	private Transform myTransform;
+	private Camera mainCam;
+	private int id_walking = Animator.StringToHash("IsWalking");
+	
 
-	void Awake()
+    private void OnEnable()
+    {
+		StaticInput.controls.Enable();
+		StaticInput.controls.Player.Movement.performed += Movement;
+        StaticInput.controls.Player.Movement.canceled += Movement;
+    }
+
+    void Awake()
 	{
 		floorMask = LayerMask.GetMask("Floor");
 		anim = GetComponent<Animator>();
 		playerRigidbody = GetComponent<Rigidbody>();
+		myTransform = transform;
+		mainCam = Camera.main;
 	}
 
 	void FixedUpdate()
 	{
-		float h = Input.GetAxisRaw("Horizontal");
-		float v = Input.GetAxisRaw("Vertical");
+		//float h = Input.GetAxisRaw("Horizontal");
+		//float v = Input.GetAxisRaw("Vertical");
 
-		Move(h, v);
+		Move(movementInput.x, movementInput.y);
 		Turning();
-		Animating(h, v);
+		Animating(movementInput.x, movementInput.y);
 	}
+
+	void Movement(InputAction.CallbackContext dir)
+	{
+        float h = dir.ReadValue<Vector2>().x;
+        float v = dir.ReadValue<Vector2>().y;
+
+        movementInput.x = h;
+		movementInput.y = v;
+
+		if (dir.canceled)
+		{
+			movementInput = Vector2.zero;
+		}
+
+    }
 
 	void Move(float h, float v)
 	{
 		movement.Set(h, 0f, v);
 		movement = movement.normalized * speed * Time.deltaTime;
 
-		playerRigidbody.MovePosition(transform.position + movement);
+		playerRigidbody.MovePosition(myTransform.position + movement);
 	}
 
 	void Turning()
 	{
-		Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+		Ray camRay = mainCam.ScreenPointToRay(Input.mousePosition);
 		RaycastHit floorHit;
 
 		if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask)) {
@@ -53,6 +83,6 @@ public class PlayerMovement : MonoBehaviour
 	{
 		bool walking = h != 0f || v != 0f;
 
-		anim.SetBool("IsWalking", walking);
+		anim.SetBool(id_walking, walking);
 	}
 }
